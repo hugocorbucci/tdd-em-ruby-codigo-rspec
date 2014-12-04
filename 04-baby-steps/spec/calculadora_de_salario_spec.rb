4bb94f372a80dc944a63bafb06da9049e98d26fe
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 08:38:54 -0900

    Voltando ao primeiro teste para reimplementar direito

--- 04-baby-steps/spec/calculadora_de_salario_spec.rb
+++ 04-baby-steps/spec/calculadora_de_salario_spec.rb
@@ -11,20 +11,4 @@ describe CalculadoraDeSalario do
 
     expect(salario).to eq(1500 * 0.9)
   end
-  it 'deve calcular salario para desenvolvedores com salario acima do limite' do
-    calculadora = CalculadoraDeSalario.new
-    desenvolvedor = Funcionario.new('Mauricio', 4000.0, Cargo::DESENVOLVEDOR)
-
-    salario = calculadora.calcula_salario desenvolvedor
-
-    expect(salario).to eq(4000 * 0.8)
-  end
-  it 'deve calcular salario para DBAs com salario abaixo do limite' do
-    calculadora = CalculadoraDeSalario.new
-    desenvolvedor = Funcionario.new('Mauricio', 500.0, Cargo::DBA)
-
-    salario = calculadora.calcula_salario desenvolvedor
-
-    expect(salario).to eq(500 * 0.85)
-  end
 end
