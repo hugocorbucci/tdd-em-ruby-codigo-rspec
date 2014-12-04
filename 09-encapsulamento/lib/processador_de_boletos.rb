5ef9393c3bca728924ad8074ab79833a5d9ba053
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 11:26:04 -0900

    Passando teste de boleto único.

--- 09-encapsulamento/lib/processador_de_boletos.rb
+++ 09-encapsulamento/lib/processador_de_boletos.rb
@@ -3,10 +3,15 @@ require_relative './pagamento.rb'
 
 class ProcessadorDeBoletos
   def processa boletos, fatura
-    boleto = boletos[0]
-    pagamento = 
-      Pagamento.new boleto.valor, MeioDePagamento::BOLETO
-    fatura.pagamentos << pagamento
+    valor_total = 0
+    boletos.each do |boleto|
+      pagamento = Pagamento.new boleto.valor, MeioDePagamento::BOLETO
+      fatura.pagamentos << pagamento
+
+      valor_total += boleto.valor
+    end
+
+    fatura.paga = true if valor_total >= fatura.valor
   end
 end
 
