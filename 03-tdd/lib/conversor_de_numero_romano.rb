be056e25f8153d202eb0896242f14d77226f419c
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-03 16:00:13 -0900

    Passando teste para V com implementação de if

--- 03-tdd/lib/conversor_de_numero_romano.rb
+++ 03-tdd/lib/conversor_de_numero_romano.rb
@@ -1,6 +1,11 @@
 #encoding: UTF-8
 class ConversorDeNumeroRomano
   def converte numero_em_romano
-    1
+    if numero_em_romano == 'I'
+      return 1
+    elsif numero_em_romano == 'V'
+      return 5
+    end
+    return 0 
   end
 end
