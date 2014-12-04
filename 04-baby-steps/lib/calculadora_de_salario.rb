6a2b359cd8b08f2013c415add854e6851b6c05f2
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 08:39:23 -0900

    Implementação mais inteligente

--- 04-baby-steps/lib/calculadora_de_salario.rb
+++ 04-baby-steps/lib/calculadora_de_salario.rb
@@ -1,10 +1,6 @@
 #encoding: UTF-8
 class CalculadoraDeSalario
   def calcula_salario funcionario
-    if funcionario.cargo == Cargo::DESENVOLVEDOR
-      return 3200.0 if funcionario.salario > 3000
-      return 1350.0
-    end
-    return 425.0
+    return 1500 * 0.9
   end
 end
