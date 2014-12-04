155f13b2bd51a038b2b8e9547eb4aea0e2161197
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 09:45:42 -0900

    E adicionando clausula para testador

--- 07-coesao/lib/calculadora_de_salario.rb
+++ 07-coesao/lib/calculadora_de_salario.rb
@@ -4,7 +4,8 @@ class CalculadoraDeSalario
     if funcionario.cargo == Cargo::DESENVOLVEDOR
       return funcionario.salario * 0.8 if funcionario.salario > 3000
       return funcionario.salario * 0.9
-    elsif funcionario.cargo == Cargo::DBA
+    elsif funcionario.cargo == Cargo::DBA ||
+            funcionario.cargo == Cargo::TESTADOR
       return funcionario.salario * 0.85 if funcionario.salario < 2500
       return funcionario.salario * 0.75
     end
