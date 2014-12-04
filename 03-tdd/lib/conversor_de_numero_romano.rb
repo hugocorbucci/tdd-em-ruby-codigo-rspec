11471e5d7b6d68e131ed278eb9e8971c068ab565
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-03 16:05:56 -0900

    Refatorando com abordagem funcional

--- 03-tdd/lib/conversor_de_numero_romano.rb
+++ 03-tdd/lib/conversor_de_numero_romano.rb
@@ -11,10 +11,8 @@ class ConversorDeNumeroRomano
     'M' => 1000,
   }
   def converte numero_em_romano
-    acumulador = 0
-    numero_em_romano.split('').each do |digito_em_romano|
-      acumulador += TABELA[digito_em_romano]
-    end
-    acumulador
+    numero_em_romano.split('').
+      map{|digito| TABELA[digito]}.
+      reduce(:+)
   end
 end
