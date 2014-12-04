7d292277633e441665f94a076cc022b476fba32d
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 09:00:44 -0900

    Extraindo setup dos testes com repetição

--- 06-cuidando-dos-testes/spec/carrinho_de_compras_spec.rb
+++ 06-cuidando-dos-testes/spec/carrinho_de_compras_spec.rb
@@ -3,23 +3,22 @@ require_relative '../lib/item.rb'
 require_relative '../lib/carrinho_de_compras.rb'
 
 describe CarrinhoDeCompras do
+  before :each do
+    @carrinho = CarrinhoDeCompras.new
+  end
   it 'deve retornar zero se carrinho vazio' do
-    carrinho = CarrinhoDeCompras.new
-
-    expect(carrinho.maior_valor).to eq(0)
+    expect(@carrinho.maior_valor).to eq(0)
   end
   it 'deve retornar valor do item se carrinho com 1 elemento' do
-    carrinho = CarrinhoDeCompras.new
-    carrinho << Item.new('Geladeira', 1, 900.0)
+    @carrinho << Item.new('Geladeira', 1, 900.0)
 
-    expect(carrinho.maior_valor).to eq(900.0)
+    expect(@carrinho.maior_valor).to eq(900.0)
   end
   it 'deve retornar maior valor se carrinho contem muitos elementos' do
-    carrinho = CarrinhoDeCompras.new
-    carrinho << Item.new('Geladeira', 1, 900.0)
-    carrinho << Item.new('Fogão', 1, 1500.0)
-    carrinho << Item.new('Máquina de Lavar', 1, 750.0)
+    @carrinho << Item.new('Geladeira', 1, 900.0)
+    @carrinho << Item.new('Fogão', 1, 1500.0)
+    @carrinho << Item.new('Máquina de Lavar', 1, 750.0)
 
-    expect(carrinho.maior_valor).to eq(1500.0)
+    expect(@carrinho.maior_valor).to eq(1500.0)
   end
 end
