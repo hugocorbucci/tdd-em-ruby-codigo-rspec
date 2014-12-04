86758ea8da3d7154e583303b237f88123a6504ee
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 09:09:56 -0900

    Melhorando testes para calculadora com classes de equivalencia

--- 06-cuidando-dos-testes/spec/calculadora_spec.rb
+++ 06-cuidando-dos-testes/spec/calculadora_spec.rb
@@ -2,13 +2,20 @@
 require_relative '../lib/calculadora.rb'
 
 describe Calculadora do
-  it 'deve somar um mais um' do
-    expect(Calculadora.new.soma(1,1)).to eq(2)
-  end
-  it 'deve somar um mais dois' do
-    expect(Calculadora.new.soma(1,2)).to eq(3)
-  end
-  it 'deve somar dois mais dois' do
+  it 'deve somar dois numeros positivos' do
     expect(Calculadora.new.soma(2,2)).to eq(4)
+  end
+  it 'deve somar positivo com negativo' do
+    expect(Calculadora.new.soma(6,-2)).to eq(4)
+  end
+  it 'deve somar negativo com positivo' do
+    expect(Calculadora.new.soma(-6,2)).to eq(-4)
+  end
+  it 'deve somar dois numeros negativos' do
+    expect(Calculadora.new.soma(-2,-2)).to eq(-4)
+  end
+  it 'deve somar com zero' do
+    expect(Calculadora.new.soma(0,4)).to eq(4)
+    expect(Calculadora.new.soma(4,0)).to eq(4)
   end
 end
