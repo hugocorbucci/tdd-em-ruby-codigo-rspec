a0c077123c0b755724e62a17ca6ba0d1b448df93
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 08:38:20 -0900

    Passando teste para DBA com mais if

--- 04-baby-steps/lib/calculadora_de_salario.rb
+++ 04-baby-steps/lib/calculadora_de_salario.rb
@@ -1,7 +1,10 @@
 #encoding: UTF-8
 class CalculadoraDeSalario
   def calcula_salario funcionario
-    return 3200.0 if funcionario.salario > 3000
-    return 1350.0
+    if funcionario.cargo == Cargo::DESENVOLVEDOR
+      return 3200.0 if funcionario.salario > 3000
+      return 1350.0
+    end
+    return 425.0
   end
 end
