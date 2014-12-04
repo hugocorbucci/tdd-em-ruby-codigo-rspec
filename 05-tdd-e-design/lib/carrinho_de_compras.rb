aa5fb6a3170e05d63ef54c5d24a3db2e32b9ba67
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 08:52:19 -0900

    Refatorando para que maior valor esteja no carrinho

--- 05-tdd-e-design/lib/carrinho_de_compras.rb
+++ 05-tdd-e-design/lib/carrinho_de_compras.rb
@@ -8,4 +8,13 @@ class CarrinhoDeCompras
   def <<(item)
     itens << item
   end
+  def maior_valor
+    return 0 if itens.empty?
+
+    maior = itens[0].valor_total
+    itens.each do |item|
+      maior = item.valor_total if maior < item.valor_total
+    end
+    maior
+  end
 end
--- 05-tdd-e-design/spec/carrinho_de_compras_spec.rb
+++ 05-tdd-e-design/spec/carrinho_de_compras_spec.rb
@@ -1,25 +1,18 @@
 #encoding: UTF-8
 require_relative '../lib/item.rb'
 require_relative '../lib/carrinho_de_compras.rb'
-require_relative '../lib/maior_preco.rb'
 
-describe MaiorPreco do
+describe CarrinhoDeCompras do
   it 'deve retornar zero se carrinho vazio' do
     carrinho = CarrinhoDeCompras.new
 
-    algoritmo = MaiorPreco.new
-    valor = algoritmo.encontra carrinho
-
-    expect(valor).to eq(0)
+    expect(carrinho.maior_valor).to eq(0)
   end
   it 'deve retornar valor do item se carrinho com 1 elemento' do
     carrinho = CarrinhoDeCompras.new
     carrinho << Item.new('Geladeira', 1, 900.0)
 
-    algoritmo = MaiorPreco.new
-    valor = algoritmo.encontra carrinho
-
-    expect(valor).to eq(900.0)
+    expect(carrinho.maior_valor).to eq(900.0)
   end
   it 'deve retornar maior valor se carrinho contem muitos elementos' do
     carrinho = CarrinhoDeCompras.new
@@ -27,9 +20,6 @@ describe MaiorPreco do
     carrinho << Item.new('Fogão', 1, 1500.0)
     carrinho << Item.new('Máquina de Lavar', 1, 750.0)
 
-    algoritmo = MaiorPreco.new
-    valor = algoritmo.encontra carrinho
-
-    expect(valor).to eq(1500.0)
+    expect(carrinho.maior_valor).to eq(1500.0)
   end
 end
