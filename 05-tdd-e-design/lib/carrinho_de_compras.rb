0b67911f2b45576430e74a8a72eac514d440f5e1
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 08:52:52 -0900

    Refatorando para abordagem funcional

--- 05-tdd-e-design/lib/carrinho_de_compras.rb
+++ 05-tdd-e-design/lib/carrinho_de_compras.rb
@@ -9,12 +9,6 @@ class CarrinhoDeCompras
     itens << item
   end
   def maior_valor
-    return 0 if itens.empty?
-
-    maior = itens[0].valor_total
-    itens.each do |item|
-      maior = item.valor_total if maior < item.valor_total
-    end
-    maior
+    itens.map(&:valor_total).max || 0
   end
 end
