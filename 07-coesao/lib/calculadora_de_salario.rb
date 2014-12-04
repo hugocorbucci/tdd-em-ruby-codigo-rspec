94983495bda3a1bfc9a8561d53d651e8cd275e1c
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 09:47:17 -0900

    Refatorando para métodos privados

--- 07-coesao/lib/calculadora_de_salario.rb
+++ 07-coesao/lib/calculadora_de_salario.rb
@@ -2,13 +2,21 @@
 class CalculadoraDeSalario
   def calcula_salario funcionario
     if funcionario.cargo == Cargo::DESENVOLVEDOR
-      return funcionario.salario * 0.8 if funcionario.salario > 3000
-      return funcionario.salario * 0.9
+      return dez_ou_vinte_por_cento_de_desconto funcionario
     elsif funcionario.cargo == Cargo::DBA ||
             funcionario.cargo == Cargo::TESTADOR
-      return funcionario.salario * 0.85 if funcionario.salario < 2500
-      return funcionario.salario * 0.75
+      return quinte_ou_vinte_cinco_por_cento_de_desconto funcionario
     end
     raise Exception.new('Funcionario invalido')
+  end
+
+  private
+  def dez_ou_vinte_por_cento_de_desconto funcionario
+    return funcionario.salario * 0.8 if funcionario.salario > 3000
+    return funcionario.salario * 0.9
+  end
+  def quinte_ou_vinte_cinco_por_cento_de_desconto funcionario
+    return funcionario.salario * 0.85 if funcionario.salario < 2500
+    return funcionario.salario * 0.75
   end
 end
