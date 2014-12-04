240cc325ffab7957faafa4724a354cd67bea56f8
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 10:30:09 -0900

    Adicionando chamada ao NFDao

--- 08-acoplamento/lib/gerador_de_nota_fiscal.rb
+++ 08-acoplamento/lib/gerador_de_nota_fiscal.rb
@@ -1,9 +1,14 @@
 #encoding: UTF-8
 require_relative './nota_fiscal.rb'
+require_relative './nf_dao.rb'
 
 class GeradorDeNotaFiscal
   def gera(pedido)
-    NotaFiscal.new pedido.cliente,
+    nf = NotaFiscal.new pedido.cliente,
       pedido.valor_total * 0.94, Time.now
+
+    NFDao.new.persiste nf
+
+    nf
   end
 end
