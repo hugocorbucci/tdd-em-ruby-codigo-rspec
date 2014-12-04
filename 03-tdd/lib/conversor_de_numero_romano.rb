598c9da919f11e647f093b4d54eaa20a5f42dc03
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-03 16:10:49 -0900

    Passando teste para IX

--- 03-tdd/lib/conversor_de_numero_romano.rb
+++ 03-tdd/lib/conversor_de_numero_romano.rb
@@ -11,8 +11,22 @@ class ConversorDeNumeroRomano
     'M' => 1000,
   }
   def converte numero_em_romano
-    numero_em_romano.split('').
-      map{|digito| TABELA[digito]}.
-      reduce(:+)
+    acumulador = 0
+    ultimo_vizinho_da_direita = 0
+    numero_em_romano.split('').reverse.each do |digito_em_romano|
+      # pega o inteiro referente ao simbolo atual
+      atual = TABELA[digito_em_romano]
+
+      # se o da direita for menor, o multiplicaremos
+      # por -1 para torná-lo negativo
+      multiplicador = 1
+      multiplicador = -1 if(atual < ultimo_vizinho_da_direita)
+
+      acumulador += TABELA[digito_em_romano] * multiplicador
+
+      # atualiza o vizinho da direita
+      ultimo_vizinho_da_direita = atual
+    end
+    acumulador
   end
 end
