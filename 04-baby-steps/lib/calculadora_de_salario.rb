6f6ed4e14a4ded3b1202a54de54d2542818ff453
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 08:41:06 -0900

    Passando teste de salario de dev acima do limite

--- 04-baby-steps/lib/calculadora_de_salario.rb
+++ 04-baby-steps/lib/calculadora_de_salario.rb
@@ -1,6 +1,7 @@
 #encoding: UTF-8
 class CalculadoraDeSalario
   def calcula_salario funcionario
+    return funcionario.salario * 0.8 if funcionario.salario > 3000
     return funcionario.salario * 0.9
   end
 end
