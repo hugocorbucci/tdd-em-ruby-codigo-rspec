02eb79b5d0f2195d8bb23c8d2e4adab4a6eff0c2
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 08:50:12 -0900

    Passando teste iterando pelos itens

--- 05-tdd-e-design/lib/maior_preco.rb
+++ 05-tdd-e-design/lib/maior_preco.rb
@@ -2,6 +2,13 @@
 class MaiorPreco
   def encontra carrinho
     return 0 if carrinho.itens.empty?
-    return carrinho.itens[0].valor_total
+
+    maior = carrinho.itens[0].valor_total
+    carrinho.itens.each do |item|
+      if maior < item.valor_total
+        maior = item.valor_total
+      end
+    end
+    maior
   end
 end
