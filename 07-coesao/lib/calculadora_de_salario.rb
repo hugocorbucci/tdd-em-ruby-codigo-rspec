9d3d62cd742ee5b065339f4c0cb35dd705b67485
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-12-04 09:45:01 -0900

    Passando teste para cargo de DBA

--- 07-coesao/lib/calculadora_de_salario.rb
+++ 07-coesao/lib/calculadora_de_salario.rb
@@ -1,7 +1,13 @@
 #encoding: UTF-8
 class CalculadoraDeSalario
   def calcula_salario funcionario
-    return funcionario.salario * 0.8 if funcionario.salario > 3000
-    return funcionario.salario * 0.9
+    if funcionario.cargo == Cargo::DESENVOLVEDOR
+      return funcionario.salario * 0.8 if funcionario.salario > 3000
+      return funcionario.salario * 0.9
+    elsif funcionario.cargo == Cargo::DBA
+      return funcionario.salario * 0.85 if funcionario.salario < 2500
+      return funcionario.salario * 0.75
+    end
+    raise Exception.new('Funcionario invalido')
   end
 end
